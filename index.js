f49d43a837a3c58f43a8fbef145563448f293169
Sean MacIsaac <seanjmacisaac@gmail.com>
2016-12-07 10:15:35 -0500

    Fixed missing 'return' statement

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = function requireResolver(file) {
   try {
-    require(path.resolve(file));
+    return require(path.resolve(file));
   } catch (e) {
     return function (curryFile) {
       return requireResolver(path.join(file, curryFile));
