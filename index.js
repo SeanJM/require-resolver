211c56254d19fc28c2ad61c7071d28d7a3bd8c90
Sean MacIsaac <seanjmacisaac@gmail.com>
2016-12-08 08:32:51 -0500

    Added 'isError' function to better check for error messages.

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 const path = require('path');
+let root;
+
+function isError(e) {
+  let isModule = e.code === 'MODULE_NOT_FOUND';
+  let hasRoot = e.message.split(' ').slice(3).join(' ').slice(1).startsWith(root);
+  if (isModule && !hasRoot) {
+    return true;
+  }
+  if (!isModule) {
+    return true;
+  }
+  return false;
+}
 
 module.exports = function requireResolver(file) {
   try {
+    root = path.resolve(file).split(path.sep).slice(0, 2).join(path.sep);
     return require(path.resolve(file));
   } catch (e) {
-    return function (curryFile) {
-      return requireResolver(path.join(file, curryFile));
-    };
+    if (isError(e)) {
+      console.log(e.stack);
+    } else {
+      return function (curryFile) {
+        return requireResolver(path.join(file, curryFile));
+      };
+    }
   }
 };
